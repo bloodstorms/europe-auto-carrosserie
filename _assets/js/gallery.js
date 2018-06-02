29fa33387fb127aebf7c9496a5eede8e51111ba9
Tiffany VACHEZ <tiffany.vachez@toucantoco.com>
2018-06-02 12:54:12 +0200

    add gulp

---
+++ _assets/js/gallery.js
@@ -0,0 +1,63 @@
+// Gallery.js
+
+var selectedClass = "";
+
+$(".gallery__button[data-rel='all']").addClass('active');
+
+$(".gallery__button").click(function(){
+  $(".gallery__button").removeClass('active');
+  $(this).addClass('active');
+	selectedClass = $(this).attr("data-rel");
+  $(".gallery__wrapper").fadeTo(100, 0.1);
+	$(".gallery__wrapper div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+
+  setTimeout(function() {
+    $("."+selectedClass).fadeIn().addClass('scale-anm');
+    $(".gallery__wrapper").fadeTo(300, 1);
+  }, 300);
+});
+
+// Name and total number of images in each category + 1
+var categories = [
+  {'catName': 'classic-cars', 'catNumb': 12 + 1},
+  {'catName': 'super-cars', 'catNumb': 13 + 1}
+];
+
+for(var loopIndex = 0; loopIndex < categories.length; loopIndex++) {
+  for(var imgIndex = 1; imgIndex < categories[loopIndex].catNumb; imgIndex++) {
+    $('.gallery__wrapper')[0].innerHTML += '<div class="gallery__item scale-anm all ' + categories[loopIndex].catName + '"><img src="/assets/images/gallery/' + categories[loopIndex].catName + '/' + imgIndex + '.jpg" alt="" /></div>'
+  }
+}
+
+//COPYRIGHT CLICK
+$("img").on("contextmenu", function(e) {
+  e.preventDefault();
+});
+
+$(window).on('mousedown', function(e) {
+  $('#right-click-copyright').fadeOut(1)
+});
+
+$(window).on('mousedown', function(e) {
+  if (e.button == 2) {
+
+    var axisX = e.pageX;
+    var axisY = e.pageY;
+
+    $('#right-click-copyright').fadeIn(100);
+    $('#right-click-copyright').css({
+      "left": axisX,
+      "top": axisY
+    });
+
+    e.stopPropagation();
+    e.preventDefault();
+
+    setTimeout(function(){
+      $('#right-click-copyright').fadeOut(1)
+    }, 2000);
+
+  } else {
+    return false;
+  }
+});
